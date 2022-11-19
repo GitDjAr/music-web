9e16a89182c7b96f4e617dc56baf46df643ffeed
GitDjAr <15984278781@163.com>
2022-11-20 00:01:28 +0800

    feat:404  专辑

---
+++ src/views/404/index.vue
@@ -0,0 +1,15 @@
+<!--  -->
+<template>
+  <div class=''>404</div>
+</template>
+
+<script lang='ts' setup>
+import { ref, reactive, } from 'vue'
+const state = reactive({
+  count: 0,
+})
+
+</script>
+<style scoped lang='scss'>
+
+</style>
