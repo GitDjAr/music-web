a7c4c2885e1c2b8c1e4ff9a306ff166a38c987a7
xdj <xdj773750101@gamil.com>
2022-11-23 16:46:33 +0800

    更新:404page

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,14 +1,15 @@
 <!--  -->
 <template>
   <div class="flex flex-col  justify-center items-center">
-    <img :style="{ width: '1200px' }" src="../../assets/404.png" alt="" />
-    <div>
+    <img :style="{ width: '780px' }" :src="url" alt="" />
+    <div class=" mt-5">
       <a-button type="primary" @click="go('/Music/Home')" class=" mr-2">回到首页</a-button>
       <a-button type="primary" @click="go('')">回到上一页</a-button>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router';
 const Router = useRouter()
 const go = (path: string) => {
@@ -18,4 +19,6 @@ const go = (path: string) => {
     Router.replace(path)
   }
 }
+
+const url: string = ref(window.rendomImgurl)
 </script>
