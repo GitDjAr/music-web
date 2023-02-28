81fa1e9fa19e166d7c0f74b4b4c6c54790dcc59f
xdj <xdj773750101@gamil.com>
2023-02-28 17:58:48 +0800

    feat: 专辑页

--- src/components/album/index.vue
+++ src/components/album/index.vue
@@ -1,24 +1,12 @@
 <!--  -->
 <template>
-  <div
-    @click="toPage"
-    class="mybox cursor-pointer group hover:bg-sky-50 hover:shadow-lg transition-all flex w-96 h-36 px-3 m-2 text-center shadow rounded-md items-center"
-  >
+  <div @click="toPage"
+    class="mybox cursor-pointer group hover:bg-sky-50 hover:shadow-lg transition-all flex w-96 h-36 px-3 m-2 text-center shadow rounded-md items-center">
     <div class="relative w-32 h-32">
-      <Image
-        :wh="[200, 200]"
-        :src="P.item?.blurPicUrl"
-        alt=""
-        class="rounded-md w-32 h-32 absolute -translate-y-1/2 top-1/2 z-10"
-      />
-      <div
-        class="group-hover:translate-x-14 -translate-y-1/2 top-1/2 transition-all absolute"
-      >
-        <img
-          class="changping w-28 h-28"
-          src="../../assets/changpian.png"
-          alt=""
-        />
+      <Image :wh="[200, 200]" :src="P.item?.blurPicUrl" alt=""
+        class="rounded-md w-32 h-32 absolute -translate-y-1/2 top-1/2 z-10" />
+      <div class="group-hover:translate-x-14 -translate-y-1/2 top-1/2 transition-all absolute">
+        <img class="changping w-28 h-28" src="../../assets/changpian.png" alt="" />
       </div>
     </div>
     <div class="h-4/5 text-left ml-14 flex flex-col justify-around">
@@ -47,7 +35,7 @@ const P = defineProps<{
 }>();
 
 function toPage() {
-  router.push({ name: "albums", params: { id: P.item.id } });
+  router.push({ name: "playlist", params: { id: P.item.id } });
 }
 </script>
 <style scoped lang="scss">
