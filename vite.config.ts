2cdd0ef5669b0ba066178dc898cfeb52f5cef389
xdj <xdj773750101@gamil.com>
2023-05-08 10:55:21 +0800

    feat: 环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -58,7 +58,7 @@ export default defineConfig({
   },
   server: {
     port: 4000,
-    host: "localhost",
+    host: '0.0.0.0',
     proxy: {
       "/api/": {
         target: "http://localhost:3000",
