59bc9bda681337ac905501ae301492ee9f67481e
xdj <xdj773750101@gamil.com>
2023-05-12 15:07:51 +0800

    feat: test post-commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,10 +4,10 @@
 </template>
 
 <script lang='ts' setup>
-// import { ref, reactive, } from 'vue'
-// const state = reactive({
-//   count: 0,
-// })
+import { ref, reactive, } from 'vue'
+const state = reactive({
+  count: 0,
+})
 
 </script>
 <style scoped lang='scss'></style>
